3351d0d482cca89809ab616b2fdf7c0f1e7d52fe
Aleksandr Kozminykh <sa.kozminykh@gmail.com>
2026-03-14 14:56:23 +0100

    feat(scrollback): implementation and tests

--- lib/src/main/kotlin/org/example/scrollback/ScrollBack.kt
+++ lib/src/main/kotlin/org/example/scrollback/ScrollBack.kt
@@ -2,10 +2,11 @@ package org.example.scrollback
 
 import org.example.models.Cell
 import org.example.models.Line
+import org.example.models.SomeCell
 
 internal interface ScrollBack {
     fun line(lineNumber: Int): Line
     val content: List<Line>
 
-    fun cellAt(row: Int, column: Int): Cell
+    fun cellAt(row: Int, column: Int): SomeCell
 }
